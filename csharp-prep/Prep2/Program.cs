4adc506972b98e051a8f0633fd15c3af128d26dd
vickneshsuryia <vickneshsuryia@gmail.com>
2025-09-20 11:55:07 -0600

    9/20's prep 1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your test score? ");
+        string score = Console.ReadLine();
+        int grade = int.Parse(score);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+
+        Console.WriteLine($"This is your grade: {letter}.");
+        
+        if (letter == "D" || letter == "F")
+        {
+            Console.WriteLine("You unfortunately did not pass the test. Try again next time!");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
     }
 }
