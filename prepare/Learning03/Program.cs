65e5ce861fb5241a0eb25f24b67ac1dc6798737d
vickneshsuryia <vickneshsuryia@gmail.com>
2025-10-16 20:57:38 -0600

    10/16 learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        fraction1.SetTop(1);
+        fraction1.SetBottom(1);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        Fraction fraction2 = new Fraction();
+        fraction2.SetTop(5);
+        fraction2.SetBottom(1);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+        Fraction fraction3 = new Fraction();
+        fraction3.SetTop(3);
+        fraction3.SetBottom(4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+        Fraction fraction4 = new Fraction();
+        fraction4.SetTop(1);
+        fraction4.SetBottom(3);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
